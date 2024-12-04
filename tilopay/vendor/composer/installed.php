1b35b83a3e2cd72cc455ed583f171256078ec082
Sean Murphy <sean@iamseanmurphy.com>
2024-12-04 02:42:50 +0000

    3.0.7

---
+++ tilopay/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'tilopay/tilopay-plugin',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => '3f03054dbac97f4344baeb078c25a03045fcfe72',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'tilopay/tilopay-plugin' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => '3f03054dbac97f4344baeb078c25a03045fcfe72',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
